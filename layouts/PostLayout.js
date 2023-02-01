eb8197c7808dd69ab9501fb6b2b2c82135cc677b
michaelsebast <michael@frans,web.id>
2023-02-01 09:22:03 +0700

    fix github link

--- layouts/PostLayout.js
+++ layouts/PostLayout.js
@@ -8,7 +8,8 @@ import siteMetadata from '@/data/siteMetadata'
 import Comments from '@/components/comments'
 import ScrollTopAndComment from '@/components/ScrollTopAndComment'
 
-const editUrl = (fileName) => `https://github.com/michaelsebast/michaelsebastian.my.id/edit/main/data/blog/${fileName}`
+const editUrl = (fileName) =>
+  'https://github.com/michaelsebast/michaelsebastian.my.id/edit/main/data/blog/${fileName}'
 const discussUrl = (slug) =>
   `https://mobile.twitter.com/search?q=${encodeURIComponent(
     `${siteMetadata.siteUrl}/blog/${slug}`
