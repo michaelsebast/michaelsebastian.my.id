2628bca5b8ef5beb5699e5d5bf0ff21e69f10507
harleydica <taufiklah87@gmail.com>
2023-02-08 07:43:40 +0700

    typo url

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://taufikcrisnawan.dev',
+  siteUrl: 'https://michaelsebastian.my.id/',
   generateRobotsTxt: true,
   robotsTxtOptions: {
     policies: [{ userAgent: '*', allow: '/' }],
